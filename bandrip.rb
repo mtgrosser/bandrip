4399d65a925c240b58a5eed3d26edf3003e81389
Matthias Grosser <mtgrosser@gmx.net>
2020-09-25 12:02:05 +0200

    Update for latest bandcamp as of Sept 2020

--- bandrip.rb
+++ bandrip.rb
@@ -1,7 +1,11 @@
-# encoding: utf-8
+#!/usr/bin/env ruby
+
+# bandrip.rb v0.2.0
+# 2020-09-25
 
 require 'uri'
 require 'net/http'
+require 'json'
 require 'bundler/inline'
 
 gemfile do
@@ -11,14 +15,24 @@ gemfile do
 end
 
 class Bandrip
+  VERSION = '0.2.0'
   USER_AGENT = 'Mozilla/5.0 (Android; Mobile; rv:40.0) Gecko/40.0 Firefox/40.0'
-  MP3_REGEX = /\{\"mp3-128\"\:\"(\S+)\"\}/mi
   WAIT_TIME = 10
   
   CHARACTER_FILTER = /[\x00-\x1F\/\\:\*\?\"<>\|]/u
   UNICODE_WHITESPACE = /[[:space:]]+/u
   
   class << self
+    def usage
+      puts <<~USAGE
+        Usage: ruby bandrip.rb URL
+        
+        Single track URL: https://artist-name.bandcamp.com/track/track-name
+        Full album URL:   https://artist-name.bandcamp.com/album/album-name
+        
+      USAGE
+    end
+  
     def from_url(url)
       uri = URI(url)
       raise ArgumentError, "not a valid Bandcamp URL" unless uri.host.end_with?('.bandcamp.com')
@@ -64,7 +78,9 @@ class Bandrip
     filename = sanitize_filename("#{artist_name} - #{track_name}.mp3")
     number = number.to_i
     filename = "#{'%02d' % number} #{filename}" if number > 0
-    raise 'No MP3 link found' unless mp3_url = MP3_REGEX.match(document.to_s).captures.first
+    track_data = JSON.parse(document.at_css('script[data-tralbum]')['data-tralbum'])
+    mp3_url = track_data['trackinfo'].first.dig('file', 'mp3-128')
+    raise 'No MP3 link found' unless mp3_url && mp3_url.start_with?('https://')
     print "  #{filename} ... "
     if File.exist?(filename)
       puts "already exists, skipping!"
@@ -99,9 +115,18 @@ class Bandrip
    def parse_track_name(document)
      document.at_css('#name-section .trackTitle').text.strip
    end
+
+   def sleep_with_info(seconds)
+     print " "
+     seconds.times do
+       print "."
+       sleep 1
+     end
+     print " "
+   end
    
    def http_get(url, referrer = nil)
-     sleep WAIT_TIME
+     sleep_with_info WAIT_TIME
      uri = URI.parse(url)
      http = Net::HTTP.new(uri.host, uri.port)
      http.use_ssl = true
@@ -122,4 +147,21 @@ class Bandrip
    end
 end
 
-Bandrip.from_url(ARGV[0]) if $PROGRAM_NAME == __FILE__
+if $PROGRAM_NAME == __FILE__
+  begin
+    arg = ARGV[0]
+    if arg.nil? || arg == '' || arg == '--help'
+      Bandrip.usage
+      exit(1)
+    elsif arg == '--version'
+      puts "bandrip.rb v#{Bandrip::VERSION}\n"
+      exit(0)
+    else
+      Bandrip.from_url(ARGV[0])
+    end
+  rescue => e
+    puts "Error: #{e.message}"
+    puts "Check bandrip.rb --help for usage\n"
+    exit(1)
+  end
+end
